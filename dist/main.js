4e4b393b844b0a08f88f9364ab8551e6a8cb3014
Samir <bsam@bk.ru>
2021-11-15 17:56:44 +0300

    Webpack first configuration

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+var age = 22;
+console.log(age);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsSUFBTSxHQUFHLEdBQVcsRUFBRSxDQUFDO0FBQ3ZCLE9BQU8sQ0FBQyxHQUFHLENBQUMsR0FBRyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zbGlkZXIvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgYWdlOiBudW1iZXIgPSAyMjtcclxuY29uc29sZS5sb2coYWdlKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
