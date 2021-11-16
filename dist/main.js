ff6f3844b9905ea9432de3e07fb24b7507929b80
Samir <bsam@bk.ru>
2021-11-16 13:53:54 +0300

    Add webpack configs for work with pug and scss

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.ts ***!
-  \**********************/
+/*!******************!*\
+  !*** ./index.ts ***!
+  \******************/
 
 var age = 22;
 console.log(age);
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsSUFBTSxHQUFHLEdBQVcsRUFBRSxDQUFDO0FBQ3ZCLE9BQU8sQ0FBQyxHQUFHLENBQUMsR0FBRyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zbGlkZXIvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgYWdlOiBudW1iZXIgPSAyMjtcclxuY29uc29sZS5sb2coYWdlKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsSUFBTSxHQUFHLEdBQVcsRUFBRSxDQUFDO0FBQ3ZCLE9BQU8sQ0FBQyxHQUFHLENBQUMsR0FBRyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBhZ2U6IG51bWJlciA9IDIyO1xyXG5jb25zb2xlLmxvZyhhZ2UpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './index.ts',
     devtool: 'inline-source-map',
+    context: path.resolve(__dirname, 'src'),
+    plugins: [
+        new HtmlWebpackPlugin({
+            'filename': 'index.html',
+            'template': './index.pug'
+        }),
+        new CleanWebpackPlugin
+    ],
     module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.pug$/,
+                    use: 'pug-loader?pretty=true'
+                },
+                {
+                    test: /\.css$/,
+                    use:  [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: '' }
+                    },
+                    'css-loader']
+                },
+                {
+                    test: /\.s[ac]ss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: { publicPath: '' }
+                        },
+                        'css-loader',
+                        'sass-loader']
+                },
+            ],
+    },
+    resolve: {
         extensions: ['.tsx', '.ts', '.js'],
-      },
+    },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
 };
